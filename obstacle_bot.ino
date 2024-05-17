fb3616ef1b6d1cfff545a15fba8ffe5ca1ee863e
Clarence Jay Fetalino <102894582+ahley18@users.noreply.github.com>
2024-05-17 23:05:34 +0800

    Add files via upload

---
+++ obstacle_bot.ino
@@ -0,0 +1,56 @@
+const int trigPin = 11;
+const int echoPin = 10;
+const int in1 = 9;
+const int in2 = 8;
+const int in3 = 4;
+const int in4 = 3;
+
+int duration = 0;
+int distance = 0;
+
+void setup() 
+{
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode (in1, OUTPUT);
+  pinMode (in2, OUTPUT);
+  pinMode (in3, OUTPUT);
+  pinMode (in4, OUTPUT);
+
+  Serial.begin(9600);
+}
+//long duration, distance;
+
+void loop()
+{     
+  //digitalWrite(trigPin, LOW);
+  //delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);  
+
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2)/28.5;
+  Serial.println(distance);
+
+  
+  if (distance < 20)
+    {
+      digitalWrite(in1, LOW); 
+      digitalWrite(in2, HIGH); 
+      digitalWrite(in3, HIGH); 
+      digitalWrite(in4, LOW);
+      delay(100);
+    }
+  else
+    {
+      digitalWrite(in1, HIGH); 
+      digitalWrite(in2, LOW); 
+      digitalWrite(in3, HIGH); 
+      digitalWrite(in4, LOW);
+      
+    }  
+  //delay(50);
+}
